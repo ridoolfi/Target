abc2e1054eee2947052454676320df519636928f
ridoolfi <angeloggc2@gmail.com>
2024-09-13 23:34:21 -0300

    Comentario resposta

--- question 1.js
+++ question 1.js
@@ -1,8 +1,3 @@
-//1)	Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0; 
-//Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
-//Imprimir(SOMA); 
-//Ao final do processamento, qual será o valor da variável SOMA? 
-
 let indice = 13;
 let soma = 0; 
 let k = 0;
@@ -11,4 +6,4 @@ while(k < indice){
     k += 1;
     soma += k;
 }
-console.log(soma);
+console.log(soma); //91
